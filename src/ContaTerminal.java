c50732947f466570919cb7ccea501c4709703f3b
REISENBERG77 <carvalhoreiseduardo815@gmail.com>
2025-04-08 17:47:00 -0300

    Adicionado tratamento de exceções e um loop

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -6,18 +7,38 @@ public class ContaTerminal {
         String agencia;
         String Nome_Cliente;
         double saldo;
+        boolean ValidarEntrada= false;
+
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o numero");
-        numero=scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("Digete a agencia. Ex. xxx-x");
-        agencia = scanner.nextLine();
-        System.out.println("Digite o nome do cliente");
-        Nome_Cliente = scanner.nextLine();
-        System.out.println("Digite o saldo da conta");
-        saldo = scanner.nextDouble();
-        System.out.println("Ola " + Nome_Cliente + ", obrigado por criar uma conta em nosso banco,sua agencia é " + agencia + ",conta " + numero + " e seu saldo " + saldo + " esta disponivel para saque");
+        do {
+            try {
+                System.out.println("Digite o numero");
+                numero = scanner.nextInt();
+                scanner.nextLine();
 
+                System.out.println("Digete a agencia. Ex. xxx-x");
+                agencia = scanner.nextLine();
+                if (!agencia.matches("\\d{3}-\\d")) {
+                    throw new IllegalArgumentException("Agência inválida! Use o formato xxx-x");
+                }
+
+                System.out.println("Digite o nome do cliente");
+                Nome_Cliente = scanner.nextLine();
+                if (Nome_Cliente.matches(".*\\d.*")) {
+                    throw new IllegalArgumentException("Nome inválido! Não pode conter números.");
+                }
+                System.out.println("Digite o saldo da conta");
+                saldo = scanner.nextDouble();
+                ValidarEntrada=true;
+                System.out.println("Ola " + Nome_Cliente + ", obrigado por criar uma conta em nosso banco,sua agencia é " + agencia + ",conta " + numero + " e seu saldo " + saldo + " esta disponivel para saque");
+
+            } catch (InputMismatchException e) {
+                System.out.println("Digite um valor numerico");
+                scanner.nextLine();
+            }catch (IllegalArgumentException e){
+                System.out.println("erro" + e.getMessage());
+            }
+        }while (!ValidarEntrada);
     }
 }
